4f6e6073941747a496973cfa2d079d720e3264e9
lll9p <lll9p.china@gmail.com>
2014-04-17 00:04:04 +0800

    update pngs

--- dots2svg.py
+++ dots2svg.py
@@ -20,7 +20,7 @@ def convert(svg_data,file,x,y,r,color,line_height):
     for c,l in svg_data:
         svg_string += '<circle cx="'+str(l*x)+'" cy="'+str(c*y)+'" r="'+str(r)+'" fill="'+color+'"/>'+'\n'
         svg_string += '<circle cx="'+str(l*x)+'" cy="'+str((c+line_height)*y)+'" r="'+str(r)+'" fill="'+color+'"/>'+'\n'
-    svg_string = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' + '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="' + str((c+4)*y) + '" width="' + str(l*x*2) + '"><rect x="-2000%" y="-2000%" height="4000%" width="4000%" fill="black"/>' + svg_string + '</svg>'
+    svg_string = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n' + '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="' + str((c+4)*y) + '" width="' + str(l*x*2) + '">\n<desc>Ingress logos - By lll9p, No rights reserved, lll9p.china@gmail.com, http://imtho.com</desc>\n<rect x="-2000%" y="-2000%" height="4000%" width="4000%" fill="black"/>\n' + svg_string + '</svg>'
     with open(file,'w') as f:
         f.write(svg_string)
 
