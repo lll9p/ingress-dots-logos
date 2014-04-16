c05f13d8f93371749445d34eaa9ec8a4b537e4e8
lll9p <lll9p.china@gmail.com>
2014-04-16 23:50:26 +0800

    first update

---
+++ dots2svg.py
@@ -0,0 +1,31 @@
+import linecache
+'''
+convert dots to svg
+'''
+def get_svg_data(schar,file):
+    ascii_logo = linecache.getlines(file)
+    svg_data = []
+    ln = 0
+    for line in ascii_logo:
+        cn = 0
+        for char in line:
+            if char == schar:
+                svg_data.append((ln,cn))
+            cn+=1
+        ln+=1
+    return svg_data
+
+def convert(svg_data,file,x,y,r,color,line_height):
+    svg_string = ''
+    for c,l in svg_data:
+        svg_string += '<circle cx="'+str(l*x)+'" cy="'+str(c*y)+'" r="'+str(r)+'" fill="'+color+'"/>'+'\n'
+        svg_string += '<circle cx="'+str(l*x)+'" cy="'+str((c+line_height)*y)+'" r="'+str(r)+'" fill="'+color+'"/>'+'\n'
+    svg_string = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' + '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="' + str((c+4)*y) + '" width="' + str(l*x*2) + '"><rect x="-2000%" y="-2000%" height="4000%" width="4000%" fill="black"/>' + svg_string + '</svg>'
+    with open(file,'w') as f:
+        f.write(svg_string)
+
+if __name__ == '__main__':
+    hDw = 19/26
+    convert(get_svg_data(':','Resistance'),'Resistance.svg',19*hDw,19,1.8,'#0592d0',0.5)
+    convert(get_svg_data(':','Enlightened'),'Enlightened.svg',19*hDw,19,1.8,'#02bf02',0.5)
+    convert(get_svg_data(':','Ingress'),'Ingress.svg',19*hDw,19,1.8,'#0592d0',0.5)
